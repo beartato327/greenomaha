da17cca1b06b7431f63382947c1c48ec64b3f578
beartato327 <rcapurro@unomaha.edu>
2022-03-03 11:33:24 -0600

    logo update

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,10 +1,9 @@
 <template>
-  <v-app-bar height="120px" color="green" elevation="3" flat>
+  <v-app-bar height="110px" color="white" elevation="3" flat>
     <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
  --><v-img
-      max-height="110px"
+      max-height="100px"
       max-width="300px"
-      
       src="../assets/logo.png"
     ></v-img>
     <v-toolbar-title class="white--text" ></v-toolbar-title>
