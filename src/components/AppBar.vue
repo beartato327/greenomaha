5f316dfd916ff3f282936b68780201e3b34f1228
beartato327 <rcapurro@unomaha.edu>
2022-03-16 18:22:40 -0500

    Update AppBar.vue

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,13 +1,23 @@
 <template>
-  <v-app-bar height="110px" color="white" elevation="3" flat>
-    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <div>
+    <v-app-bar height="100%" color="white" elevation="3" flat>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
  --><v-img
-      max-height="100px"
-      max-width="300px"
-      src="../assets/logo.png"
-    ></v-img>
-    <v-toolbar-title class="white--text" ></v-toolbar-title>
-  </v-app-bar>
+        id="logo"
+        max-height="100px"
+        max-width="300px"
+        src="../assets/logo.png"
+      ></v-img>
+    </v-app-bar>
+    <v-app-bar height="40%" text>
+      <v-toolbar-title id="subMenu"
+        ><v-btn id="subMenuBtn" class="green--text">Who We Are</v-btn
+        ><v-btn id="subMenuBtn" class="green--text"
+          >Contact</v-btn
+        ></v-toolbar-title
+      >
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
@@ -15,4 +25,15 @@ export default {};
 </script>
 
 <style>
+#subMenuBtn:hover {
+  background-color: #64c44447 !important;
+}
+#logo {
+  display: block;
+  margin: 0 auto;
+}
+#subMenu {
+  display: block;
+  margin: 0 auto;
+}
 </style>
