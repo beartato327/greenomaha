bc2de898ce35612c53db5d61e7a5c5a6363331be
beartato327 <recapurro@outlook.com>
2022-03-23 14:48:54 -0500

    Updates to name and hefty

--- src/components/Hefty.vue
+++ src/components/Hefty.vue
@@ -7,15 +7,15 @@
         >
         <v-card-text
           >The energy bag program is supported throughout the Greater Omaha
-          Area. You can purchase these bags from most grocery and home improvement stores, to se a
+          Area. You can purchase these bags from most grocery and home improvement stores, to see a
           complete list click
           <a
             href="https://www.hefty.com/products/hefty-energybag/energybag-communities/nebraska/greater-omaha-program"
             >here</a
-          >. To see a list of what is accepted by Hefty click the button below.
+          >. To see more information about the progra visit Hefty's website below.
         </v-card-text>
         <v-card-actions> 
-            <v-btn raised class="white--text" href="https://www.hefty.com/products/hefty-energybag/how-energybag-program-works" color="orange">More Info</v-btn>
+            <v-btn raised class="white--text" href="https://www.hefty.com/products/hefty-energybag/how-energybag-program-works" color="orange">Hefty <v-icon small>mdi-lightning-bolt</v-icon></v-btn>
         </v-card-actions>
       </v-card>
 </template>
