a7075fb6cef4231d033a23b36b12eeb44821643d
beartato327 <rcapurro@unomaha.edu>
2022-03-25 17:40:32 -0500

    adding analytics

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,15 +1,21 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, logEvent } from "firebase/analytics";
 
 const firebaseApp = initializeApp({
-    apiKey: "AIzaSyAIsSu95HFq-ZqN8m70__UkBnL4DduGxj0",
-    authDomain: "greenomaha-137ec.firebaseapp.com",
-    projectId: "greenomaha-137ec",
-    storageBucket: "greenomaha-137ec.appspot.com",
-    messagingSenderId: "1024398843752",
-    appId: "1:1024398843752:web:a7a6f1356470955256c0dd",
-    measurementId: "G-ZE13R9710B"
-  });
+  apiKey: "AIzaSyAIsSu95HFq-ZqN8m70__UkBnL4DduGxj0",
+  authDomain: "greenomaha-137ec.firebaseapp.com",
+  projectId: "greenomaha-137ec",
+  storageBucket: "greenomaha-137ec.appspot.com",
+  messagingSenderId: "1024398843752",
+  appId: "1:1024398843752:web:a7a6f1356470955256c0dd",
+  measurementId: "G-ZE13R9710B",
+});
 
-export const db = getFirestore(firebaseApp)
+const analytics = getAnalytics(firebaseApp);
 
+logEvent(analytics, 'first_visit')
+logEvent(analytics, 'page_view')
+
+
+export const db = getFirestore(firebaseApp);
